0e585d163234a74837085e764f9160f35bd7985f
Peter Ndumia <ndegwandumia@gmail.com>
2025-03-29 09:35:48 -0400

    Clean up

--- frontend/src/components/ChatWindow.js
+++ frontend/src/components/ChatWindow.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { Box, Paper, Typography, Avatar } from '@mui/material';
+import { Box} from '@mui/material';
 import Message from './Message';
 
 export default function ChatWindow({ messages }) {
